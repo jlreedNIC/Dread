ee8cf8e674ac4082f496a60887283ce98e063825
Jordan Reed <reed5204@vandals.uidaho.edu>
2022-09-28 16:44:30 -0700

    Started base weapon class. Added bullet class. Bullets now despawn on collision, but not after a set time yet.

---
+++ Assets/src/Jordan/Base_Weapon.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base_Weapon : MonoBehaviour
+{
+    [SerializeField] private Transform firePoint; // where bullets spawn from
+
+    [SerializeField] private float bulletForce = 20f;
+
+    [SerializeField] private string weapon_name;
+    [SerializeField] private float fire_rate;
+    [SerializeField] private int weapon_dmg_mod;
+    [SerializeField] private int fire_range;
+
+    [SerializeField] private GameObject bullet_type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // likely to be in player script
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+        
+    }
+
+    // currently spawns a bullet traveling in the upwards direction
+    void Fire()
+    {
+        GameObject bullet = Instantiate(bullet_type, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        // rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse); // to use after mouse input added
+
+        rb.AddForce(new Vector2(0.0f, bulletForce), ForceMode2D.Impulse);
+    }
+}
--- Assets/src/Jordan/bullet.cs
+++ Assets/src/Jordan/bullet.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
+    [SerializeField] private float total_damage;
+    [SerializeField] private Transform starting_point;
+    [SerializeField] private int distance;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // set starting point when bullet is spawned by base weapon
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // either here or in fixed update, check amount of distance allowed to travel
+        // if dist. traveled is max then destroy game object
+        // think about setting up collider for range that gets set with each new weapon
+        // on collider exit, (or collide with enemy) destroy object
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("damage: " + total_damage);
+        // call other.health -= total_damage; ????
+        Destroy(gameObject);
     }
 }
