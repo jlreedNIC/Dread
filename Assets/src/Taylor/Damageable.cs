c3808766db42908a206531c508b7bd444a4df8cf
TaylorM <tmartin6894@gmail.com>
2022-10-10 09:29:32 -0700

    created testing scene in Taylor Scene folder

--- Assets/src/Taylor/Damageable.cs
+++ Assets/src/Taylor/Damageable.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class Damageable : MonoBehaviour 
 {
+    //base health to be loaded in or set in editor 
     [SerializeField] public int baseHealth;
 
+    //variable to hold and modify current health value
     [SerializeField] public int currentHealth;  
 
+    //Start
+    //init curret health as sent in game object base health
     private void Start()
     {
         currentHealth = baseHealth;
-
     }
 
+    //TakeDamage() 
+    //makes a game object take damage to thier health
+    //clamps value so it cannot go negative
     public void TakeDamage(int damageDealt)
     {
         currentHealth = Mathf.Clamp(currentHealth - damageDealt, 0, baseHealth);
@@ -22,7 +28,19 @@ public class Damageable : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+    }
 
+    //GainHealth() 
+    //makes a game object gain health
+    //clamps value so it cannot go over base health
+    public void GainHealth(int healthObtained)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + healthObtained, 0, baseHealth);
+
+        if(currentHealth >= baseHealth)
+        {
+            currentHealth = baseHealth;
+        }
     }
 
     
