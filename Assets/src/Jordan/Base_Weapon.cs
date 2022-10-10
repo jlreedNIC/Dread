16476ad4b035b818b852661439d1dbdf4312ff01
Jordan Reed <reed5204@vandals.uidaho.edu>
2022-10-09 20:30:13 -0700

    Added a fire rate to weapons. Also now able to switch the active weapon and pick up a new weapon

--- Assets/src/Jordan/Base_Weapon.cs
+++ Assets/src/Jordan/Base_Weapon.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+    TO DO:
+        decorator
+ */
+
 public class Base_Weapon : MonoBehaviour
 {
     [SerializeField] private Transform firePoint; // where bullets spawn from
@@ -16,23 +21,26 @@ public class Base_Weapon : MonoBehaviour
     // gets set with the ammo manager
     [SerializeField] private GameObject bullet_type;
 
+    [SerializeField] private bool canFire;
+    [SerializeField] private bool canBePickedUp;
+
     // audio manager
     // should we have this? or just call the script Dan is making???
-    [SerializeField] private AudioManager AudioMan;
+    // [SerializeField] private AudioManager AudioMan;
 
     // Start is called before the first frame update
     public void Start()
     {
         // set audio manager variable to play the correct sounds
-        AudioMan = FindObjectOfType<AudioManager>();
-        if(AudioMan)
-        {
-            Debug.Log("Found audio manager");
-        }
-        else
-        {
-            Debug.Log("Did not find audio manager");
-        }
+        // AudioMan = FindObjectOfType<AudioManager>();
+        // if(AudioMan)
+        // {
+        //     Debug.Log("Found audio manager");
+        // }
+        // else
+        // {
+        //     Debug.Log("Did not find audio manager");
+        // }
 
         // get firepoint if not set
         if(firePoint == null)
@@ -43,40 +51,98 @@ public class Base_Weapon : MonoBehaviour
 
         // base range
         fire_range = 5;
-
+        canFire = true;
+        canBePickedUp = true;
     }
 
     // Update is called once per frame
     public void Update()
     {
-        // likely to be in player script
-        // if(Input.GetButtonDown("Fire1"))
-        // {
-        //     Fire();
-        //     /* Plays Audio clip for gun firing on player clicks.
-        //     NOTE: This feature will need to be adjusted for other weapon types later in develoment
-        //     as they will have alternate sound bites. DB*/
-        //     // FindObjectOfType<AudioManager>().Play("Pew");
-        // }
         
     }
 
     // currently spawns a bullet traveling in the upwards direction
     public void Fire()
     {
-        // play pewpew sound
-        if(AudioMan)
+        if(canFire)
         {
-            Debug.Log("Playing pewpew sound!");
-            AudioMan.Play("Pew");
-        }
+            // play pewpew sound
+            // FindObjectOfType<AudioManager>().Play("Pew");
+            // if(AudioMan)
+            // {
+            //     Debug.Log("Playing pewpew sound!");
+            //     AudioMan.Play("Pew");
+            // }
+            // else
+            // {
+            //     Debug.Log("can't play pewpew");
+            // }
+        
+            canFire = !canFire;
 
-        // spawn bullet
-        GameObject bullet = Instantiate(bullet_type, firePoint.position, firePoint.rotation);
-        // set bullet distance able to travel
-        bullet.GetComponent<bullet>().setFireRange(fire_range);
-        // apply force to bullet to move
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse); 
+            // bullet info will be set in ammo manager class
+            // spawn bullet
+            GameObject bullet = Instantiate(bullet_type, firePoint.position, firePoint.rotation);
+            // set bullet distance able to travel
+            bullet.GetComponent<bullet>().setFireRange(fire_range);
+            // apply force to bullet to move
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse); 
+
+            StartCoroutine(FireCooldown());
+        }
+        else
+        {
+            Debug.Log("can't fire yet");
+        }
+    }
+
+    // weapon can only be fired so often
+    // this function operates as the cooldown and will not let weapon be fired until ready
+    private IEnumerator FireCooldown()
+    {
+        yield return new WaitForSeconds(fire_rate);
+        canFire = true;
+        yield break;
+    }
+
+    // switch which weapon the player is carrying
+    // takes a GameObject parameter (must be of type base_class (which may be changed) )
+    // returns the new weapon GameObject
+    public GameObject SwitchActiveWeapon(GameObject oldWeapon)
+    {
+        if(canBePickedUp)
+        {
+            canBePickedUp = false;
+
+            Debug.Log("switching weapons");
+            // give the new player the position, rotation, and parent of the old weapon
+            transform.position = oldWeapon.transform.position;
+            transform.rotation = oldWeapon.transform.rotation;
+            transform.parent = oldWeapon.transform.parent;
+
+            // remove the parent and rotation of the old weapon
+            oldWeapon.transform.parent = null;
+            // oldWeapon.transform.position += new Vector3(2,0,0);
+            oldWeapon.transform.rotation = new Quaternion(0,0,0,0);
+            // oldWeapon.GetComponent<Base_Weapon>().canBePickedUp = false;
+
+            // return the new weapon to be set in the player script
+            return this.gameObject;
+        }
+        else
+        {
+            Debug.Log("can't switch with this weapon right now");
+            return oldWeapon;
+        }        
+    }
+
+    // when player leaves the old weapon behind, then it can be picked up again
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            canBePickedUp = true;
+        }
     }
 }
